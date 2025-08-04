8386cfe2faafc702bd2f74092467750fb6921649
Jesson Mondejar <mondejarjessonnoel@smcbi.edu.ph>
2025-08-04 16:25:07 +0800

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
+  readonly VITE_GOOGLE_CLIENT_ID: string
+  readonly VITE_GOOGLE_CLIENT_SECRET: string
 }
 
 interface ImportMeta {
